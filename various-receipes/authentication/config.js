98a95e3ad8352d538833bd5a50c02b37f2d54ffe
Yoni <me@goldbergyoni.com>
2020-10-11 08:28:30 +0300

    Genric message

--- various-receipes/authentication/config.js
+++
@@ -1,7 +0,0 @@
-//This simulates production configuration provider where keys are put in JSON and/or
-//in env vars
-
-module.exports = {
-    JWTSecret: 'secret'
-}
-process.env.JWT_SECRET = 'secret';
