c7befa8c12b17f997a386bef3a9f5efef24f3e19
Yoni <me@goldbergyoni.com>
2022-08-23 15:06:13 +0300

    Genric message

--- recipes/consumer-driven-contract-test/add-order.provider-verification.test.js
+++ recipes/consumer-driven-contract-test/add-order.provider-verification.test.js
@@ -6,3 +6,11 @@ const opts = {
 new Verifier(opts).verifyProvider().then(function () {
     // do something
 });
+
+
+providerBaseUrl: 'http://localhost:3001'await new Verifier({
+    providerBaseUrl: 'http://localhost:8080',
+    pactUrls: [path.resolve(__dirname, './pacts/orderclient-orderapi.json')],
+  }).verifyProvider()
+
+  publishVerificationResult: true
